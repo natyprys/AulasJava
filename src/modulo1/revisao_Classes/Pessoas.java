b1a18e181441949f4bfda73fab82a9ec302a43ed
Nathalia Assuncao <nathalia.p.lins@gmail.com>
2021-09-29 14:43:30 -0300

    Atividade Finalizada

--- src/modulo1/revisao_Classes/Pessoas.java
+++ src/modulo1/revisao_Classes/Pessoas.java
@@ -3,4 +3,6 @@ package modulo1.revisao_Classes;
 public class Pessoas {
     String nome;
     String sobrenome;
+    int idade;
+
 }
--- src/modulo1/revisao_Classes/Principal.java
+++ src/modulo1/revisao_Classes/Principal.java
@@ -1,5 +1,18 @@
 package modulo1.revisao_Classes;
 
-public class Principal {
+import modulo1.aula011.Calculadora;
 
+public class Principal {
+    public static void main(String[] args) {
+        Pessoas pessoas = new Pessoas();
+        pessoas.nome = "Nathalia";
+        pessoas.sobrenome = "Priscila";
+        pessoas.idade = 25;
+
+        System.out.println(pessoas.nome);
+        System.out.println(pessoas.sobrenome);
+        System.out.println(pessoas.idade);
+
+
+    }
 }
