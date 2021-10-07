bd11c242f7ae5410ca51f076dc5e2d2bab6189db
Nathalia Assuncao <nathalia.p.lins@gmail.com>
2021-10-07 16:23:44 -0300

    Aulas 16

---
+++ src/modulo1/aula016/P1/Pessoa.java
@@ -0,0 +1,25 @@
+package modulo1.aula016.P1;
+
+public class Pessoa {
+    private String nome;
+    private String sobrenome;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    @Override
+    public String toString() {
+        String mensagem = this.nome + " " + this.sobrenome;
+        return mensagem;
+    }
+}
---
+++ src/modulo1/aula016/P3/Tela2.java
@@ -0,0 +1,12 @@
+package modulo1.aula016.P3;
+
+public class Tela2 {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa("Maykon", "Grenemann");
+        Pessoa p2 = new Pessoa("Dyego", "Rauen");
+        Pessoa p3 = new Pessoa("Inez", "Brasil");
+        System.out.println( p1 );
+        System.out.println( p2 );
+        System.out.println( p3 );
+    }
+}
