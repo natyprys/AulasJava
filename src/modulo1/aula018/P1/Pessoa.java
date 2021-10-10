2ddb3253f005a38f9ca0eda02c3b4ea37689736e
Nathalia Assuncao <nathalia.p.lins@gmail.com>
2021-10-10 17:09:01 -0300

    Aula 18 - P1

---
+++ src/modulo1/aula018/P1/Pessoa.java
@@ -0,0 +1,12 @@
+package modulo1.aula018.P1;
+
+public class Pessoa {
+    private String nomeCompleto;
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+}
