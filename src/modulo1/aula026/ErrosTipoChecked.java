4c8d9ee9d3b371c1d9d02a3429b298a00fa95724
Nathalia Assuncao <nathalia.p.lins@gmail.com>
2021-10-25 17:43:07 -0300

    Aula 26

---
+++ src/modulo1/aula026/ErrosTipoChecked.java
@@ -0,0 +1,15 @@
+package modulo1.aula026;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ErrosTipoChecked {
+    public static void main(String[] args) {
+        File arquivo = new File("teste.txt");
+        try {
+            arquivo.createNewFile();
+        } catch (IOException e) {
+            System.out.println("Deu ruim");
+        }
+    }
+}
