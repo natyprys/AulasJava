bdeada794df82f09eff1ee3ae782405552840b8f
Nathalia Assuncao <nathalia.p.lins@gmail.com>
2021-11-13 12:35:45 -0300

    arrumando packages

--- src/Atividades/AtividadePratica28/Controller/EnderecoController.java
+++ src/Atividades/AtividadePratica28/Controller/EnderecoController.java
@@ -1,7 +1,6 @@
-package modulo1.Atividades.AtividadePratica28.Controller;
+package Atividades.AtividadePratica28.Controller;
 
-import modulo1.Atividades.AtividadePratica28.Models.Endereco;
-import modulo1.Atividades.AtividadePratica29.Controller.BaseController;
+import Atividades.AtividadePratica29.Controller.BaseController;
 
-public class EnderecoController extends BaseController<Endereco> {
+public class EnderecoController extends BaseController<modulo1.Atividades.AtividadePratica28.Models.Endereco> {
 }
