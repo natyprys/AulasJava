08a0fe6c5a3b50f5ab4b81da743bb7acbba4ec2a
Nathalia Assuncao <nathalia.p.lins@gmail.com>
2021-11-19 22:01:10 -0300

    Atividade 48 Finalizada

---
+++ src/Atividades/AtividadePratica48/View/Delete.java
@@ -0,0 +1,29 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class Delete {
+    public static void main(String[] args) {
+        try {
+        int id = 2;
+
+        Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "123456");
+        
+        String sql = "DELETE FROM categoria WHERE id_categoria = 1" ; 
+        PreparedStatement prepStatement = conn.prepareStatement(sql);
+           
+
+        prepStatement.execute();   
+        int linhasAfetadas = prepStatement.getUpdateCount();
+        System.out.println(linhasAfetadas);         
+        prepStatement.setInt(1, id);    
+
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/Atividades/AtividadePratica48/View/Update.java
@@ -0,0 +1,33 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class Update {
+    public static void main(String[] args) {
+        
+    
+    try {
+        String nome = "copaa";
+        int id = 7;
+
+        Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "123456");
+        
+        String sql = "UPDATE categoria SET nome=? WHERE id_categoria = ?";         
+        PreparedStatement prepStatement = conn.prepareStatement(sql);
+        prepStatement.setString(1, nome);
+        prepStatement.setInt(2, id);
+
+        prepStatement.execute();  
+ 
+        int linhasAfetadas = prepStatement.getUpdateCount();
+        System.out.println(linhasAfetadas);
+
+        conn.close();
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+}
+}
