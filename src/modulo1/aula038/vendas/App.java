649fcad062659d5e7bd1a0d5121f16a3e794322c
Nathalia Assuncao <nathalia.p.lins@gmail.com>
2021-11-23 23:45:45 -0300

    Aula comentada

---
+++ src/modulo1/aula038/vendas/App.java
@@ -0,0 +1,13 @@
+package com.natyprys.vendas;
+
+import javax.swing.JFrame;
+
+import com.natyprys.vendas.model.CategoriaView;
+
+public class App {
+    public static void main( String[] args ){
+    	 CategoriaView vc2 = new CategoriaView();
+         vc2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);      
+        
+    }
+}
