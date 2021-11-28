d4be60d5028a7054742cdf7fb5250046dee15b26
Nathalia Assuncao <nathalia.p.lins@gmail.com>
2021-11-28 18:36:47 -0300

    Atividade concluida

---
+++ src/Atividades/AtividadePratica54/vendas/src/main/java/com/natyprys/Atividade54/App.java
@@ -0,0 +1,12 @@
+package com.natyprys.Atividade54;
+
+import javax.swing.JFrame;
+
+import com.natyprys.Atividade54.view.CategoriaView;
+
+public class App {
+    public static void main( String[] args ){
+    	 CategoriaView vc2 = new CategoriaView();
+         vc2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);          
+    }
+}
