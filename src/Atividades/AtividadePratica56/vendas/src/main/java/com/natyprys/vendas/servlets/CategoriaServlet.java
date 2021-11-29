1eeed2cab7dcbad67bb5484f2ed195be6386cb35
Nathalia Assuncao <nathalia.p.lins@gmail.com>
2021-11-29 15:54:36 -0300

    Atividade 56 finalizada

---
+++ src/Atividades/AtividadePratica56/vendas/src/main/java/com/natyprys/vendas/servlets/CategoriaServlet.java
@@ -0,0 +1,36 @@
+package com.natyprys.vendas.servlets;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import com.natyprys.vendas.dao.CategoriaDao;
+import com.natyprys.vendas.models.Categoria;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet(urlPatterns = "/categoria")
+public class CategoriaServlet extends HttpServlet{
+    @Override
+    protected void doPost (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        CategoriaDao dao =new CategoriaDao();        
+        Categoria model = new Categoria();
+        
+        model.setNome(req.getParameter("nome"));
+        model.setDescricao(req.getParameter("descricao"));
+
+        model.setId(dao.insert(model));
+
+        PrintWriter out = resp.getWriter();
+        out.printf("Categoria Salva com sucesso XML- Id gerado %d", model.getId());
+    }
+
+   
+
+//  @Override protected void service - aceita get e post  
+//  @Override protected void doPost -só aceita quando usamos method="post" no jsp
+
+}
---
+++ src/Atividades/AtividadePratica56/vendas/src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="ISO-8859-1">
+		<title>Sistema de vendas</title>
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+	
+	</head>
+	<body>
+		<h1> Sistema de vendas </h1> 
+		<a href="/vendas-1/categoria.jsp">Categoria</a>
+		<a href="/vendas-1/produto.jsp">Produto</a>
+	</body>
+</html>
